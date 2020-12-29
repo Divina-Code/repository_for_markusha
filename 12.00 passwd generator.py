6f7dc31564b597a4fd7e2b4096d323175e64f052
Markushik <74201924+Markushik@users.noreply.github.com>
2020-12-29 18:35:50 +0300

    Update 12.00 passwd generator.py

--- 12.00 passwd generator.py
+++ 12.00 passwd generator.py
@@ -1,4 +1,13 @@
 import random
+Numbers = "1234567890"
+Letters = "abcdefghijklmnopqrstuvwxyz"
+BigLatters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Specialist_Symbols = "+-/*!&$#?=@<>"
 
+Length = int(input("Какова длина пароля? \n Пиши сюда цифру: "))
+passwd = " "
 
+for a in range(Length):
+   passwd += random.choice(Numbers+Letters+BigLatters+Specialist_Symbols)
+print(passwd)
 
