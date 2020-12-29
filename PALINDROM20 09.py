cd397dd4ae5b530369f1b1d4a6c0bf6c395c324a
Markushik <74201924+Markushik@users.noreply.github.com>
2020-12-29 16:09:49 +0300

    Create PALINDROM20 09.py

---
+++ PALINDROM20 09.py
@@ -0,0 +1,9 @@
+pali = input("Это проверка слова на то, является ли он палиндромом? \n Введи сюда слово:")
+pali = pali.split(" ")
+print(pali)
+for x in pali:
+    pali2 = a[::-1].lower()
+    if a.lower() == pali2
+        print("Да, бро , это палиндром: ")
+    else:
+        print("Нет, к сожалению не палиндром.")
