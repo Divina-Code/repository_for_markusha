611ecd9ef17a3272a6128dc9501830a32b00c3c4
Markushik <74201924+Markushik@users.noreply.github.com>
2021-01-04 16:07:58 +0300

    Update telegram bot.py

--- telegram bot.py
+++ telegram bot.py
@@ -1,12 +1,15 @@
 import telebot
+import random
 
 bot = telebot.TeleBot("1449912361:AAEtNTEYDxUo5H6a_pQod3ItJ9ndY7qrc5I")
 
 
 @bot.message_handler(content_types=['text'])
 def otvet_na_text(message):
-    print(message)
-    bot.send_message(message.chat.id, message.text[::-1])
+    guesses_made = 0
+    number = random.randint(1, 30)
+    id = message.chat.id
+    bot.send_message(id,"Привет, я загадал число между 1 и 30. Сможешь угадать?")
 
 
 bot.polling()
