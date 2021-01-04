efbfb9d96f297de088413298e60f2873ccb56efd
Markushik <74201924+Markushik@users.noreply.github.com>
2021-01-04 14:44:00 +0300

    Update telegram bot.py

--- telegram bot.py
+++ telegram bot.py
@@ -2,9 +2,11 @@ import telebot
 
 bot = telebot.TeleBot("1449912361:AAEtNTEYDxUo5H6a_pQod3ItJ9ndY7qrc5I")
 
+
 @bot.message_handler(content_types=['text'])
 def otvet_na_text(message):
     print(message)
     bot.send_message(message.chat.id, message.text[::-1])
+
 
 bot.polling()
