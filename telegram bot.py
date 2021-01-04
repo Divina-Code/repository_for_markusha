9c325c46ea4289ce877c4adc583b476a70e92a2d
Markushik <74201924+Markushik@users.noreply.github.com>
2021-01-04 17:44:48 +0300

    ok

--- telegram bot.py
+++ telegram bot.py
@@ -1,7 +1,8 @@
 import telebot
 import random
+from myToken import TOKEN
 
-bot = telebot.TeleBot("1449912361:AAEtNTEYDxUo5H6a_pQod3ItJ9ndY7qrc5I")
+bot = telebot.TeleBot("TOKEN")
 
 @bot.message_handler(content_types=['text'])
 def otvet_na_text(message):
@@ -12,19 +13,18 @@ def otvet_na_text(message):
     inp = message.text.lower().split()
     for word in inp:
         guesses_made < 6
-        guess = int(input('Введи число: '))
         guesses_made += 1
-        if guess < number:
+        if message < number:
             bot.send_message(id,"Твое число меньше того, что я загадал.")
 
-        if guess > number:
+        if message > number:
             bot.send_message(id,"Твое число больше загаданного мной.")
 
-        if guess == number:
+        if message == number:
             break
 
-        if guess == number:
-            bot.send_message(id,"Ух ты, {0}! Ты угадал мое число, использовав {1} попыток!")
+        if message == number:
+            bot.send_message(id,"Ух ты, ! Ты угадал мое число, использовав  попыток!")
         else:
             bot.send_message(id, "А вот и не угадал! Я загадал число" )
 
